16713b21c506b5cb47aa53b10c72befe868d1bfd
metanol596 <amaremkulov96@gmail.com>
2023-04-19 18:34:52 +0300

    change setting page

--- src/pages/GameSettings/GameSettings.tsx
+++ src/pages/GameSettings/GameSettings.tsx
@@ -3,25 +3,43 @@ import { Link } from 'react-router-dom';
 
 import s from './gameSettings.module.css';
 
-const GAME_LEVELS = [
-  { level: 'Простой', cells: '8x8', time: '10min' },
-  { level: 'Средний', cells: '16x16', time: '40min' },
-  { level: 'Сложный', cells: '32x16', time: '100min' },
+interface Level {
+  level: string;
+  cells: string;
+  mine: number;
+}
+
+const GAME_LEVELS: Level[] = [
+  { level: 'Простой', cells: '8x8', mine: 10 },
+  { level: 'Средний', cells: '16x16', mine: 40 },
+  { level: 'Сложный', cells: '32x16', mine: 100 },
 ];
 
 function GameSettings() {
-  const [currentLevel, setCurrentLevel] = useState<string>();
+  const [currentLevel, setCurrentLevel] = useState<Level>(GAME_LEVELS[0]);
+
+  const handleLevelClick = (level: Level) => {
+    setCurrentLevel(level);
+  };
 
   return (
     <section className={s.settings}>
       <h1 className={s.heading}>Уровень сложности:</h1>
       <ul className={s.list}>
         {GAME_LEVELS.map((item) => (
-          <li key={item.level} className={s.item}>
+          <li key={item.level} className={s.item} onClick={() => handleLevelClick(item)}>
             {item.level}
           </li>
         ))}
       </ul>
+      <div className={s.currLevel}>
+        <div className={s.settingItemText}>
+          Поле: <span className={s.settingText}>{currentLevel.cells}</span>
+        </div>
+        <div className={s.settingItemText}>
+          Количество мин: <span className={s.settingText}>{currentLevel.mine}</span>
+        </div>
+      </div>
       <Link to={'/game'} className={s.button}>
         Играть
       </Link>
