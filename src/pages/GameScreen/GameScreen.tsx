474e0527629e4c8bbf3b615e0f966a36f26b3789
metanol596 <amaremkulov96@gmail.com>
2023-04-19 19:17:10 +0300

    add toSettingsButton

--- src/pages/GameScreen/GameScreen.tsx
+++ src/pages/GameScreen/GameScreen.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Timer } from '../../components';
 
 import s from './gameScreen.module.css';
@@ -5,6 +6,9 @@ import s from './gameScreen.module.css';
 function GameScreen() {
   return (
     <section className={s.game}>
+      <Link to={'/'} className={s.toSettingsButton}>
+        To settings
+      </Link>
       <Timer />
     </section>
   );
