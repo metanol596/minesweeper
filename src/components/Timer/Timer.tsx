53987de4dd31b68198c710e4ce73f9e31eb5950e
metanol596 <amaremkulov96@gmail.com>
2023-04-22 16:20:40 +0300

    change Timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -3,15 +3,27 @@ import { useState, useEffect } from 'react';
 import s from './timer.module.css';
 
 function Timer() {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
 
   useEffect(() => {
-    let timer = setInterval(() => setSeconds((prev) => prev + 1), 1000);
+    let timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+
+      if (seconds === 59) {
+        setMinutes((prev) => prev + 1);
+        setSeconds(0);
+      }
+    }, 1000);
 
     return () => clearInterval(timer);
   });
 
-  return <section className={s.timer}>{seconds}</section>;
+  return (
+    <section className={s.timer}>
+      {minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds}
+    </section>
+  );
 }
 
 export default Timer;
